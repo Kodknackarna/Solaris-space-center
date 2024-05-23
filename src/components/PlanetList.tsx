11d6c602cbe63678517adcb039e6fd7206765adc
Yoe <dembouz719@gmail.com>
2024-05-23 11:01:35 +0200

    Added variable for handleMouseEnter and handleMouseLeave in PlanetList

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -3,16 +3,30 @@ import Planet from "../components/Planet";
 import { Link } from "react-router-dom";
 
 type planetProps = {
-    planets : PlanetType [],
+    planets: PlanetType[],
     updateHeaderText: (text: string) => void
 }
 
-function PlanetList ({ planets, updateHeaderText } : planetProps) {
+function PlanetList({ planets, updateHeaderText }: planetProps) {
+    const handleMouseEnter = (planetName: string) => {
+        console.log(`Mouse entered: ${planetName}`);
+    };
+
+    const handleMouseLeave = (planetName: string) => {
+        console.log(`Mouse left: ${planetName}`);
+    };
+
     return (
     <>
         {planets.map(planet => (
             <Link to={`/planet/${planet.id}`} key={planet.id}>
-            <Planet planet={planet} key={planet.id} updateHeaderText={updateHeaderText}/>
+                <Planet 
+                    planet={planet} 
+                    key={planet.id} 
+                    updateHeaderText={updateHeaderText}
+                    onMouseEnter={() => handleMouseEnter(planet.name)}
+                    onMouseLeave={() => handleMouseLeave(planet.name)}
+                />
             </Link>
         ))}
     </>
